5bc936b90cbf5e115901cb186465616768b58178
Saba <syedsaba1974@gmail.com>
2026-03-07 12:51:19 +0530

    day1_introduction.py

---
+++ day1_introduction.py
@@ -0,0 +1,19 @@
+''' Python is a high-level language.
+Pros:
+ 1. Fewer lines of code
+ 2. Provides libraries / frameworks
+ 3. Install Python
+ 4. Make sure to mark "Add Python to PATH" 
+ 5. Automatically detects var @ runtime, eliminating need for explicit declaration.
+ 6.Dynamically types and has automatic garbage collection
+    *dynamically typed - no need to specify the type of variable
+    *automatic garbage coll - unused memory is deleted '''
+
+print("Hello World")
+
+''' print() - func prints message on the screen
+"" - message btn the quotation is printed 
+
+Python uses indentation .
+four spaces per indentation level
+Python expects the indentation level to be consistent within the same block (If not indentation error occurs)'''
